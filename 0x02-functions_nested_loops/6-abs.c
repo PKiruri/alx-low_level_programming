8d7b91ecb4910ba89b89ade154a1aa3b0ac40da4
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-05 20:10:08 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,33 +1,17 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _abs - compute absolute value of integer
- *
- *   * @n: integer argument
- *
- *    * Return: absolute value
- *
- *     */
-
-
+ * _abs - compute absolute value of integer
+ * @n: integer argument
+ * Return: absolute value
+ */
 
 int _abs(int n)
-
 {
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
 
-		if (n > 0)
-
-					return (n);
-
-			else if (n < 0)
-
-						return (-n);
-
-
-
-				return (0);
-
+	return (0);
 }
