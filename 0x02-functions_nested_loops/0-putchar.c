1987d2ec2daa4dbc885cbf2072a75f591a6d78c6
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-05 12:45:33 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,20 @@
 #include "holberton.h"
 
-
-
 /**
- *
- *  * main - print Holberton, followed by new line
- *
- *   * Return: 0
- *
- *    */
-
-
+ * main - print Holberton, followed by new line
+ * Return: 0
+ */
 
 int main(void)
-
 {
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-		_putchar('H');
-
-			_putchar('o');
-
-				_putchar('l');
-
-					_putchar('b');
-
-						_putchar('e');
-
-							_putchar('r');
-
-								_putchar('t');
-
-									_putchar('o');
-
-										_putchar('n');
-
-											_putchar('\n');
-
-
-
-												return (0);
-
+	return (0);
 }
