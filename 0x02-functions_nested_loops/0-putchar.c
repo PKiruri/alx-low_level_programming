d141019a2e31747ede88ca96e6b422a30cb2f3f5
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-05 14:32:07 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,10 @@
 
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
+	_putchar('m');
 	_putchar('a');
-	_putchar('r');
+	_putchar('i');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
