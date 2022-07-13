12cccb97629bec1ae4ef3b7ff440094157148c09
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-13 10:24:44 +0300

    Update 4-print_rev.c
    prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,63 +1,32 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_rev - prints a string, in reverse, followed by a new line
- *
- *   * @s: string to reverse
- *
- *    */
-
-
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to reverse
+ */
 
 void print_rev(char *s)
-
 {
 
+	int len = 0;
+	int i;
 
+	/* iterate to find length of string and point to last character */
+	while (*s != '\0')
+	{
+		len++;
+		++s;
+	}
 
-		int len = 0;
+	/* go back to character before null character */
+	s--;
 
-			int i;
+	/* print string reversed */
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
 
-
-
-				/* iterate to find length of string and point to last character */
-
-				while (*s != '\0')
-
-						{
-
-									len++;
-
-											++s;
-
-												}
-
-
-
-					/* go back to character before null character */
-
-					s--;
-
-
-
-						/* print string reversed */
-
-						for (i = len; i > 0; i--)
-
-								{
-
-											_putchar(*s);
-
-													s--;
-
-														}
-
-
-
-							_putchar('\n');
-
+	_putchar('\n');
 }
