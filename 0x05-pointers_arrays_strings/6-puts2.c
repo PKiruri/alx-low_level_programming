594b6c4d276aeedc23313b885d749de3c213143b
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-13 10:45:05 +0300

    Update 6-puts2.c
    prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,69 +1,35 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _strlen - returns the length of a string
- *
- *   * @s: string
- *
- *    * Return: length
- *
- *     */
-
-
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
 
 int _strlen(char *s)
-
 {
+	int len = 0;
 
-		int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
-
-
-			while (*s != '\0')
-
-					{
-
-								len++;
-
-										s++;
-
-											}
-
-
-
-				return (len);
-
+	return (len);
 }
 
-
-
 /**
- *
- *  * puts2 - prints every second character of a string, followed by a new line
- *
- *   * @str: string to print
- *
- *    */
-
-
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
 
 void puts2(char *str)
-
 {
+	int i;
 
-		int i;
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
 
-
-
-			for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
-
-						_putchar(str[i]);
-
-
-
-				_putchar('\n');
-
+	_putchar('\n');
 }
