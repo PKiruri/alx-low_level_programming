c02a37abdf25d5415a9881985035cf0cc6b39935
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-13 10:50:27 +0300

    Update 7-puts_half.c
     print the second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,87 +1,44 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _strlen - returns the length of a string
- *
- *   * @s: string
- *
- *    * Return: length
- *
- *     */
-
-
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
 
 int _strlen(char *s)
-
 {
+	int len = 0;
 
-		int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
-
-
-			while (*s != '\0')
-
-					{
-
-								len++;
-
-										s++;
-
-											}
-
-
-
-				return (len);
-
+	return (len);
 }
 
-
-
 /**
- *
- *  * puts_half - prints half of a string, followed by a new line
- *
- *   * @str: string to print
- *
- *    */
-
-
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
 
 void puts_half(char *str)
-
 {
+	int idx;
+	int len = _strlen(str);
 
-		int idx;
+	/* find the index to start depending on even/odd amount of strlen */
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
 
-			int len = _strlen(str);
-
-
-
-				/* find the index to start depending on even/odd amount of strlen */
-
-				if (len % 2 != 0)
-
-							idx = (len / 2) + 1;
-
-					else
-
-								idx = (len / 2);
-
-
-
-						while (idx < len)
-
-								{
-
-											_putchar(*(str + idx));
-
-													idx++;
-
-														}
-
-							_putchar('\n');
-
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
 }
