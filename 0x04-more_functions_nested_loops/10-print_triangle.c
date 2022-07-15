635183c3bf30182be11424eb0fa50cb003a63e9b
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 12:53:15 +0300

    print triangle with #'s

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,53 +1,27 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_triangle - print traingle with #'s with given size
- *
- *   * @size: size of triangle to draw
- *
- *    */
-
-
+ * print_triangle - print triangle with #'s with given size
+ * @size: size of triangle to draw
+ */
 
 void print_triangle(int size)
-
 {
+	int height;
+	int width;
+	int draw;
 
-		int height;
+	if (size <= 0)
+		_putchar('\n');
 
-			int width;
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
 
-				int draw;
+		for (draw = 1; draw <= height; draw++)
+			_putchar('#');
 
-
-
-					if (size <= 0)
-
-								_putchar('\n');
-
-
-
-						for (height = 1; height <= size; height++)
-
-								{
-
-											for (width = 1; width <= (size - height); width++)
-
-															_putchar(' ');
-
-
-
-													for (draw = 1; draw <= height; draw++)
-
-																	_putchar('#');
-
-
-
-															_putchar('\n');
-
-																}
-
+		_putchar('\n');
+	}
 }
