6ae754ba2fb42c906be3ce06e4f659b89c1dc1de
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 10:57:55 +0300

    print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,69 +1,35 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_number - print an integer, without using long, arrays, or pointers
- *
- *   * @n: number to be printed
- *
- *    */
-
-
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
 
 void print_number(int n)
-
 {
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-		unsigned int tens, digit, positive = n;
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
 
-			double t_beg = 1;
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
 
-
-
-				if (n == 0)
-
-							_putchar('0');
-
-					else
-
-							{
-
-										if (n < 0)
-
-													{
-
-																	positive = n * -1;
-
-																				_putchar('-');
-
-																						}
-
-
-
-												while (t_beg <= positive)
-
-																t_beg *= 10;
-
-														tens = t_beg / 10;
-
-
-
-																while (tens >= 1)
-
-																			{
-
-																							digit = positive / tens;
-
-																										_putchar(digit + '0');
-
-																													positive = (positive - (tens * digit));
-
-																																tens /= 10;
-
-																																		}
-
-																	}
-
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+	}
 }
