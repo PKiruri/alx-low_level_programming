8cf1c8b54b5d5d6fc99e0a45f2cb6ff2450e64f6
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 10:23:44 +0300

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,41 +1,21 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _strcmp - compare two strings
- *
- *   * @s1: one string
- *
- *    * @s2: one string
- *
- *     * Return: int that tells num spaces in between
- *
- *      */
-
-
+ * _strcmp - compare two strings
+ * @s1: one string
+ * @s2: one string
+ * Return: int that tells num spaces in between
+ */
 
 int _strcmp(char *s1, char *s2)
-
 {
+	int i = 0;
 
-		int i = 0;
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
 
-
-
-			while (*(s1 + i) == *(s2 + i) && *(s1 + i))
-
-						i++;
-
-
-
-				if (*(s2 + i))
-
-							return (*(s1 + i) - *(s2 + i));
-
-					else
-
-								return (0);
-
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
+		return (0);
 }
