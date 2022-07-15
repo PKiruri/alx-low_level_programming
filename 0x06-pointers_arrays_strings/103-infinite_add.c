fd775ca277b422dfeb06f37e12bd193a59924a5f
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 11:37:22 +0300

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@
  * Return: sum
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
 	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
