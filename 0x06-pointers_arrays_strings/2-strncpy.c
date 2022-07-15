c4237517a96a9ae9beacc80ffcb39d247e1121b4
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 10:18:54 +0300

    copies n bytes of a source into buffer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,47 +1,24 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _strncpy - copies n bytes of a source into buffer of a destination string,
- *
- *   * @dest: copy source to this buffer
- *
- *    * @src: this is the source to copy
- *
- *     * @n: n bytes to be copied
- *
- *      * Return: copied string
- *
- *       */
-
-
+ * _strncpy - copies n bytes of a source into buffer of a destination string,
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * @n: n bytes to be copied
+ * Return: copied string
+ */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
+	int i;
 
-		int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
-
-
-			for (i = 0; i < n && src[i] != '\0'; i++)
-
-						dest[i] = src[i];
-
-				while (i < n)
-
-						{
-
-									dest[i] = '\0';
-
-											i++;
-
-												}
-
-
-
-					return (dest);
-
+	return (dest);
 }
