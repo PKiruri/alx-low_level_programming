050025994d24fae0d3c97ee5c496da871289ad3d
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 12:22:28 +0300

    print 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,29 +1,15 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_numbers - print 0 to 9
- *
- *   */
-
-
+ * print_numbers - print 0 to 9
+ */
 
 void print_numbers(void)
-
 {
+	char i;
 
-		char i;
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
 
-
-
-			for (i = '0'; i <= '9'; i++)
-
-						_putchar(i);
-
-
-
-				_putchar('\n');
-
+	_putchar('\n');
 }
