bf2ba75df9768e804562b009103cd02d1461a570
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 12:44:49 +0300

    print a square of #'s given size

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,49 +1,25 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_square - print a square of #'s given size
- *
- *   * @size: size to draw
- *
- *    */
-
-
+ * print_square - print a square of #'s given size
+ * @size: size to draw
+ */
 
 void print_square(int size)
-
 {
+	int row, column;
 
-		int row, column;
-
-
-
-			if (size > 0)
-
-					{
-
-								for (row = 1; row <= size; row++)
-
-											{
-
-															for (column = 1; column <= size; column++)
-
-																			{
-
-																								_putchar('#');
-
-																											}
-
-																		_putchar('\n');
-
-																				}
-
-									}
-
-				else
-
-							_putchar('\n');
-
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (column = 1; column <= size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
