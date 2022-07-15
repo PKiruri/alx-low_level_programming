7841c097319c4387bc641b8d47b7b2439c851bc0
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 12:30:42 +0300

    print 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,43 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * more_numbers - print 0 to 14 ten times
- *
- *   */
-
-
+ * more_numbers - print 0 to 14 ten times
+ */
 
 void more_numbers(void)
-
 {
+	char i;
+	int counter;
 
-		char i;
-
-			int counter;
-
-
-
-				for (counter = 1; counter <= 10; counter++)
-
-						{
-
-									for (i = 0; i <= 14; i++)
-
-												{
-
-																if (i / 10 > 0)
-
-																					_putchar((i / 10) + '0');
-
-																			_putchar((i % 10) + '0');
-
-																					}
-
-											_putchar('\n');
-
-												}
-
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
