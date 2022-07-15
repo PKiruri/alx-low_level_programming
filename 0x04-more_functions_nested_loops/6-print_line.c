878bb2cd0f2786f8c7378748584644c3eb098fd7
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-15 12:35:09 +0300

    print the character "_" n times

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,31 +1,16 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_line - print the character "_" n times to draw a line
- *
- *   * @n: number of characters to draw
- *
- *    */
-
-
+ * print_line - print the character "_" n times to draw a line
+ * @n: number of characters to draw
+ */
 
 void print_line(int n)
-
 {
+	int times = n;
 
-		int times = n;
+	for (times = n; times > 0; times--)
+		_putchar('_');
 
-
-
-			for (times = n; times > 0; times--)
-
-						_putchar('_');
-
-
-
-				_putchar('\n');
-
+	_putchar('\n');
 }
