5650bf483f6917e458c945eeb7ddb0387d3fe7ba
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-19 12:28:13 +0300

    main

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,23 +1,12 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * set_string - set value of pointer to a char
- *
- *   * @s: variable of type pointer
- *
- *    * @to: char
- *
- *     */
-
-
+ * set_string - set value of pointer to a char
+ * @s: variable of type pointer
+ * @to: char
+ */
 
 void set_string(char **s, char *to)
-
 {
-
-		*s = to;
-
+	*s = to;
 }
