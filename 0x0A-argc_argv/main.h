004ba34fc6287dc85c74b7c7a65643e3813dc436
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-27 11:22:23 +0300

    main

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,27 +1,14 @@
-#ifndef HOLBERTON_H
-
-#define HOLBERTON_H
-
-
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _putchar(char c);
-
-
 
 #endif
