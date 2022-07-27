6a4c248b9b072a96af0d0db651173fc72b8dc1d6
PKiruri <pkiruri06@gmail.com>
2022-07-27 03:38:31 -0700

    here

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,25 +1,96 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
 int _isupper(int c);
+
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
 int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
+int mul(int a, int b);
+
+/**
+ * print_numbers - print 0 to 9
+ */
+
+void print_numbers(void);
+
+/**
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+ */
+
+void print_most_numbers(void);
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+
+void more_numbers(void);
+
+/**
+ * print_line(int n) - print the character "_" n times to draw a line
+ * @n: number of characters to draw
+ */
+
+void print_line(int n);
+
+/**
+ * print_diagonal - print character "\" n times
+ * @n: number of characters to draw
+ */
+
+void print_diagonal(int n);
+
+/**
+ * print_square - print a square of given size
+ * @size: size to draw
+ */
+
+void print_square(int size);
+
+/**
+ * print_triangle - print traingle with #'s with given size
+ * @size: size of triangle to draw
+ */
+
+void print_triangle(int size);
+
+/**
+ * print_triangle - print traingle with #'s with given size
+ * @size: size of triangle to draw
+ */
+
+void print_triangle(int size);
+
+/**
+ * print_number = print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n);
 
 #endif
