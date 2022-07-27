7eadd49f8a0d309297fc1a7f75e78fa8cd9ddc1c
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-07-27 10:50:48 +0300

    frees a 2 dimensional grid

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,125 +1,23 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _strlen - returns the length of a string
- *
- *   * @s: string
- *
- *    * Return: length
- *
- *     */
+ * free_grid - frees a 2 dimensional grid
+ * previously created by your alloc_grid function
+ * @grid: pointer to 2D array
+ * @height: height of array
+ */
 
-
-
-int _strlen(char *s)
-
+void free_grid(int **grid, int height)
 {
+	int i;
 
-		int len = 0;
+	if (grid == NULL)
+		free(grid);
 
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
-
-			while (*s != '\0')
-
-						len++, s++;
-
-
-
-				return (len);
-
-}
-
-
-
-/**
- *
- *  * argstostr - concatenates all the arguments of your program
- *
- *   * @ac: argc
- *
- *    * @av: arguments
- *
- *     * Return: pointer to array
- *
- *      */
-
-
-
-char *argstostr(int ac, char **av)
-
-{
-
-		char *s;
-
-			int len = 0, i, j, k = 0;
-
-
-
-				if (ac == 0 || av == NULL) /* validate input */
-
-							return (NULL);
-
-
-
-					/* find length to malloc */
-
-					for (i = 0; i < ac; i++)
-
-							{
-
-										len += _strlen(av[i]);
-
-											}
-
-						len += (ac + 1); /* add space for newlines and null terminator */
-
-
-
-							/* allocate memory and free if error */
-
-							s = malloc(len * sizeof(char));
-
-
-
-								if (s == NULL)
-
-										{
-
-													free(s);
-
-															return (NULL);
-
-																}
-
-
-
-									/* insert each arg into *str */
-
-									for (i = 0; i < ac; i++)
-
-											{
-
-														for (j = 0; j < _strlen(av[i]); j++)
-
-																	{
-
-																					s[k++] = av[i][j];
-
-																							}
-
-																s[k++] = '\n';
-
-																	}
-
-
-
-										return (s);
-
+	free(grid);
 }
