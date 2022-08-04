b4e31cb33f4c511fd95e3c6beeca1dac3369350a
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-08-04 13:17:05 +0300

    main

--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -1,37 +1,19 @@
 #include "1-pi.h"
-
 #include "1-pi.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+    float a;
+    float r;
 
-	    float a;
-
-	        float r;
-
-
-
-		    r = 98;
-
-		        a = PI * r * r;
-
-			    printf("%.3f\n", a);
-
-			        return (0);
-
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
 }
