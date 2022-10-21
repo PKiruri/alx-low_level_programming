9fe75323547fa46ecac6d8bc972ecd2149f95d86
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-10-21 21:20:47 +0300

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,71 +1,36 @@
 #include "hash_tables.h"
 
-
-
 /**
+ * hash_table_print - prints a hash table
  *
- *  * hash_table_print - prints a hash table
- *
- *   *
- *
- *    * @ht: the hash table to print
- *
- *     */
-
-
+ * @ht: the hash table to print
+ */
 
 void hash_table_print(const hash_table_t *ht)
-
 {
+	hash_node_t *tmp_node = NULL;
+	unsigned long int index = 0;
+	char *comma = "";
 
-		hash_node_t *tmp_node = NULL;
+	if (ht)
+	{
+		printf("{");
+		while (index < ht->size)
+		{
+			tmp_node = ht->array[index];
 
-			unsigned long int index = 0;
+			while (tmp_node)
+			{
+				printf("%s", comma);
+				comma = ", ";
 
-				char *comma = "";
+				if (tmp_node->key)
+					printf("'%s': '%s'", tmp_node->key, tmp_node->value);
 
-
-
-					if (ht)
-
-							{
-
-										printf("{");
-
-												while (index < ht->size)
-
-															{
-
-																			tmp_node = ht->array[index];
-
-
-
-																						while (tmp_node)
-
-																										{
-
-																															printf("%s", comma);
-
-																																			comma = ", ";
-
-
-
-																																							if (tmp_node->key)
-
-																																													printf("'%s': '%s'", tmp_node->key, tmp_node->value);
-
-
-
-																																											tmp_node = tmp_node->next;
-
-																																														}
-
-																									index++;
-
-																											}
-
-														printf("}\n");
-
-															}
-
+				tmp_node = tmp_node->next;
+			}
+			index++;
+		}
+		printf("}\n");
+	}
 }
